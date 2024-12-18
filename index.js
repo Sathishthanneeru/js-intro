40aa41ad1f668cfa7bff51c7eb35c623e666b44d
Sathishthanneeru <sathishthanneeru36@gmail.com>
2024-12-18 18:10:41 +0530

    new

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+
+let username=prompt("enter ur good name")
+let domain=prompt("enter the role")
+let obj={
+    studname:username,
+    company:"karur vysya bank",
+    role:domain,
+    package:"13",
+    joindate:"oct 03"
+}
+console.log(`heartly congrulation to the ${obj.studname}.she got placed in ${obj.company} , the role is ${obj.role} with ${obj.package} LPA package and joing on ${obj.joindate}`)
