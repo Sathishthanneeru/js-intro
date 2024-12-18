75b5dac6af3a20c7fb714c099f326fd3b9d757f8
Sathishthanneeru <sathishthanneeru36@gmail.com>
2024-12-18 10:34:10 +0530

    new

---
+++ new.js
@@ -0,0 +1,9 @@
+let obj = {
+    name:"karthik",
+    cash:100000,
+    from:"hyd",
+    to:"chennai",
+    by:"car",
+    on:"monday"
+};
+console.log(`${obj.name} has travelled from ${obj.from} to ${obj.to} by a ${obj.by} on ${obj.on}.`)
